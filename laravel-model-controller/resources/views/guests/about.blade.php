a2e6590ac5c39ee6d61eab305902a79a244d8cde
devitoangelo <angelodevito4@gmail.com>
2024-05-09 16:27:18 +0200

    created file about and contacts and print in page

---
+++ laravel-model-controller/resources/views/guests/about.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="container">
+    <h1>About US Laravel</h1>
+</div>
+    
+@endsection
---
+++ laravel-model-controller/resources/views/guests/welcome.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+
+
+@section('content')
+<div class="container">
+    <h1>Hello Laravel</h1>
+</div>
+    
+@endsection
