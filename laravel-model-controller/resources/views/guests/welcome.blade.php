f9795ef96b41b112bae7870c3ddd8400b4fb5341
devitoangelo <angelodevito4@gmail.com>
2024-05-09 17:27:20 +0200

    print card db in page

--- laravel-model-controller/resources/views/guests/welcome.blade.php
+++ laravel-model-controller/resources/views/guests/welcome.blade.php
@@ -3,8 +3,41 @@
 
 
 @section('content')
-<div class="container">
-    <h1>Hello Laravel</h1>
-</div>
-    
+    <div class="container">
+        <div class="row row-cols-2 row-cols-md-3 row-cols-lg-5 g-4">
+
+            @forelse ($movies as $movie)
+                <div class="col">
+                    <div class="card" style="height: 350px">
+
+                        <div class="card-title">
+
+                            <h3> {{ $movie->title }}</h3>
+
+                            <h5>{{ $movie->original_title }}</h5>
+                        </div>
+                        <div class="card-body">
+                            <p> Nazionalità : {{ $movie->nationality }}</p>
+                            <p>Date Movies : {{ $movie->date }}</p>
+                        </div>
+                        <div>
+                            Voto : {{ $movie->vote }}
+                        </div>
+
+
+                    </div>
+                </div>
+            @empty
+                <div class="col">
+                    <p>No movie</p>
+
+                </div>
+            @endforelse
+
+        </div>
+
+
+
+
+    </div>
 @endsection
